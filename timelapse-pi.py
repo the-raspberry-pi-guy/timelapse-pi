6fbc086f366d5426665e9bc34d24193a0113f10b
the-raspberry-pi-guy <theraspberrypiguy@gmail.com>
2017-03-19 16:48:16 +0000

    Flesh out and make program work

--- timelapse-pi.py
+++ timelapse-pi.py
@@ -3,48 +3,53 @@
 # Author: Matthew Timmons-Brown (The Raspberry Pi Guy)
 # 19/03/2017
 
-import picamera
 import time
 import os
+import subprocess
 from gpiozero import PWMLED, Button
 
-image_path = "/home/pi"
+path = "/home/pi/timelapse-pi/images/img"
 pic_count = 0
-shutdown = "sudo shutdown now"
+shutdown = ["sudo", "shutdown", "now"]
+
+rate = 3000
 
 led = PWMLED(14, True, 0, 100)
 button = Button(15, False, None, 3, False)
-camera = picamera.PiCamera()
-
-camera.vflip = True
 
 def starting_flash(light):
-    light.blink(0.3, 0.3, 0, 0, 5, False)
+	light.blink(0.3, 0.3, 0, 0, 5, False)
+	light.on()
 
 def off_pulse(light):
-    light.pulse(0.5, 0.5, 5, False)
+	light.pulse(0.5, 0.5, 5, False)
 
 def when_held():
-    off_pulse(led)
-    os.system(shutdown)
+	off_pulse(led)
+	subprocess.call(shutdown)
 
-def when_pressed():
-    stop = True
+def take_pic(output_path):
+	subprocess.call(["raspistill", "-vf", "-q", "100", "-t", rate, "-o", output_path])
 
-button.when_pressed = when_pressed
 button.when_held = when_held
 
 def main():
-    stop = False
-    while True:
-        # Code start
-        starting_flash(led)
-        button.wait_for_press(None)
-        stop = False
-        while not stop:
-            new_path = path + "image%s.jpg" % pic_count
-            while os.path.exists(new_path):
-                pic_count += 1
-                new_path = path + "image%s.jpg" % pic_count
+	stop = False
+	pic_count = 0
 
-            camera.capture(new_path)
+	while True:
+		starting_flash(led)
+		button.wait_for_press(None)
+		led.off()
+		stop = False
+		while not stop:
+			new_path = (path + "%s.jpg" % pic_count)
+			while os.path.exists(new_path):
+				pic_count += 1
+				new_path = (path + "%s.jpg" % pic_count)
+				print(new_path)
+
+			take_pic(new_path)
+
+if __name__ == "__main__":
+	main()
